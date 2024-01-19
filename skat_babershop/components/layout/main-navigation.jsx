26e999478373a7ce429e18687f4894ae4d4a581e
koketsobogopane <koketsobogopanenov24@gmail.com>
2024-01-19 19:55:48 +0200

    add logo to main-nav

--- skat_babershop/components/layout/main-navigation.jsx
+++ skat_babershop/components/layout/main-navigation.jsx
@@ -1,21 +1,38 @@
-//NextJS tookit 
-import Image from "next/image"
-import Link from "next/link"
+//NextJS tookit
+import Image from 'next/image';
+import Link from 'next/link';
 
 function MainNavigation() {
-    return (
-      <>
-        <Link href={'/'}><Image src={'/images/logo/Gorge-Logo.jpg'} alt='logo' width={150} height={100}/></Link>
-        <nav>
-          <ul>
-            <li><Link href={'/'}>Home</Link></li>
-            <li><Link href={'/about'}>About Us</Link></li>
-            <li><Link href={'/services'}>Services</Link></li>
-            <li><Link href={'/pricing'}>Pricing</Link></li>
+  return (
+    <>
+      <div className=' flex bg-yellow-500 justify-between items-center p-3'>
+        <Link href={'/'} className=' pl-8'>
+          <Image
+            src={'/images/logo/logo1.png'}
+            alt="logo"
+            width={150}
+            height={100}
+          />
+        </Link>
+        <nav className=' pr-52 '>
+          <ul className=' flex justify-between items-center font-semibold text-xl gap-4'>
+            <li>
+              <Link href={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link href={'/about'}>About Us</Link>
+            </li>
+            <li>
+              <Link href={'/services'}>Services</Link>
+            </li>
+            <li>
+              <Link href={'/pricing'}>Pricing</Link>
+            </li>
           </ul>
         </nav>
-      </>
-    )
-  }
+      </div>
+    </>
+  );
+}
 
-export default MainNavigation
+export default MainNavigation;
