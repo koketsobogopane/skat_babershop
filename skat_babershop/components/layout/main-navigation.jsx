cd00f24838e49849960c0f00b2d079d264c0178a
koketsobogopane <koketsobogopanenov24@gmail.com>
2024-01-19 18:51:43 +0200

    Install tailwind css

--- skat_babershop/components/layout/main-navigation.jsx
+++ skat_babershop/components/layout/main-navigation.jsx
@@ -2,11 +2,10 @@
 import Image from "next/image"
 import Link from "next/link"
 
-
 function MainNavigation() {
     return (
       <>
-        <Link href={'/'}><Image src={'/'} alt='logo' width={150} height={100}/></Link>
+        <Link href={'/'}><Image src={'/images/logo/Gorge-Logo.jpg'} alt='logo' width={150} height={100}/></Link>
         <nav>
           <ul>
             <li><Link href={'/'}>Home</Link></li>
--- skat_babershop/pages/index.js
+++ skat_babershop/pages/index.js
@@ -1,16 +1,5 @@
 import { Inter } from 'next/font/google'
 
-//Next Toolkit
-import Link from 'next/link'
-import Image from 'next/image'
-
-//My Components
-
-
-
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
