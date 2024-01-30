470d85ba3aefe201673139909edd4144123b89ae
your_username <koketsobogopanenov24@gmail.com>
2024-01-30 15:52:28 +0200

    create servicesection  and carousel components

---
+++ skat_babershop/components/layout/service-section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServiceSection() {
+  return (
+    <div className='flex justify-center'>
+      <h2 className=' font-bold text-3xl'>Check out Some of the awesome Cuts we  have made for our clients!</h2>
+      </div>
+  )
+}
--- skat_babershop/pages/index.js
+++ skat_babershop/pages/index.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/layout/banner"
 import QuickNav from "@/components/layout/quick-nav"
+import ServiceSection from "@/components/layout/service-section"
 export default function Home() {
   return (
     <>
     <Banner />
     <QuickNav />
+    <ServiceSection />
     </>
   )
 }
