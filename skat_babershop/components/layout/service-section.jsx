9f802f89b9ac6721b436aee33e5dfff54227b9cd
your_username <koketsobogopanenov24@gmail.com>
2024-02-05 10:51:52 +0200

    Remove carousel packages

--- skat_babershop/components/layout/service-section.jsx
+++ skat_babershop/components/layout/service-section.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Carousel from '../ui/carousel'
 
 export default function ServiceSection() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex flex-col items-center'>
       <h2 className=' font-bold text-3xl'>Check out Some of the awesome Cuts we  have made for our clients!</h2>
+      
+        <Carousel/>
+
       </div>
   )
 }
--- skat_babershop/components/ui/carousel.jsx
+++ skat_babershop/components/ui/carousel.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
 
-export default function Carousel() {
+import React from 'react';
+
+const CenterMode = () => {
+  const settings = {
+    infinite: true,
+speed: 500,
+slidesToShow: 6,
+slidesToScroll: 5,
+swipeToSlide: true,
+autoplay: true,
+arrows: false,
+dots: false
+  };
+
   return (
-    <div>carousel</div>
-  )
-}
+   
+  
+      <div><h1>This should be carousel!!</h1></div>
+
+      
+  );
+};
+
+export default CenterMode;
