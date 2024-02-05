971657035a6772fd0c010e68cda395230b8be9a7
your_username <koketsobogopanenov24@gmail.com>
2024-02-05 15:41:12 +0200

    Add slides to source code

--- skat_babershop/components/layout/service-section.jsx
+++ skat_babershop/components/layout/service-section.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import Carousel from '../ui/carousel'
 
 export default function ServiceSection() {
-  
+  const slides = [
+    'slide1.jpg',
+    'slide2.jpg',
+    'slide3.jpg',
+    'slide4.jpg',
+    'slide5.jpg',
+    'slide6.jpg',
+    'slide7.jpg',
+    'slide8.jpg',
+    'slide9.jpg',
+    'slide10.jpg'
+  ]
   return (
     <div className='flex flex-col items-center'>
       <h2 className=' font-bold text-3xl'>Check out Some of the awesome Cuts we  have made for our clients!</h2>
-      
-        <Carousel slides={}/>
+       <div className=' w-[60%] m-auto p-11'>
+       <Carousel slides={slides}/>
+       </div>
+        
       </div>
   )
 }
--- skat_babershop/components/ui/carousel.jsx
+++ skat_babershop/components/ui/carousel.jsx
@@ -1,17 +1,41 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
-const Carousel = () => {
-  
+const Carousel = (props) => {
+  const [current, setCurrent] = useState(0)
   const { slides } = props
+
+  const previousSlide = () => {
+    if ( current === 0) setCurrent(slides.length - 1);
+    else setCurrent(current - 1)
+  }
+
+  const nextSlide = () => {
+    if ( current === slides.length - 1) setCurrent(0);
+    else setCurrent(current + 1)
+  }
+
   return (
-    <div>
-      <div>
+    <div className=' overflow-hidden relative '>
+      <div className={" flex transition ease-out duration-40 "}
+      style={{
+        transform: `translateX(-${current * 100 }%)`,
+      }}
+      >
         {
           slides.map((slide)=> {
-            return <img src='slide' alt='slide' />
+            return <img src={`images/slides/${slide}`} alt='slide' />
           })
         }
+      </div>
+      <div className='flex justify-between absolute top-0 h-full w-full items-center'>
+        <button type='button' onClick={previousSlide}>
+          <FaArrowCircleLeft size={50}/>
+        </button>
+        <button type='button' onClick={nextSlide}>
+          <FaArrowCircleRight size={50}/>
+        </button>
       </div>
     </div>
   );
