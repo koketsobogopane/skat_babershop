e2bd4c7fa2c83822578abc9328d97efba2a1d505
your_username <koketsobogopanenov24@gmail.com>
2024-02-20 14:58:43 +0200

    add correct formatting

--- skat_babershop/components/layout/main-navigation.jsx
+++ skat_babershop/components/layout/main-navigation.jsx
@@ -1,4 +1,4 @@
-//NextJS tookit
+//NextJS toolkit
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -7,37 +7,37 @@ import NavLink from '../ui/nav-link';
 
 //React Icons
 
-
 function MainNavigation() {
   return (
     <>
-      <div className=' sticky top-0 w-full  bg-yellow-500 z-50 p-3 mb-10'>
-      <div className=' flex justify-between items-center '>
-        <Link href={'/'} className=' pl-8'>
-          <Image
-            src={'/images/logo/logo1.png'}
-            alt="logo"
-            width={100}
-            height={50}
-          />
-        </Link>
-        <nav className=' pr-52 '>
-          <ul className=' flex justify-between items-center font-semibold text-lg gap-4'>
-            <li>
-              <NavLink href={'/'}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink href={'/about'}>About Us</NavLink>
-            </li>
-            <li>
-              <NavLink href={'/services'}>Services</NavLink>
-            </li>
-            <li>
-              <NavLink href={'/pricing'}>Pricing</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <div className=" sticky top-0 w-full  bg-yellow-500 z-50 p-3 mb-10">
+        <div className=" flex justify-between items-center ">
+          <Link href={'/'} className=" pl-8">
+            <Image
+              src={'/images/logo/logo1.png'}
+              alt="logo"
+              width={100}
+              height={50}
+              priority
+            />
+          </Link>
+          <nav className=" pr-52 ">
+            <ul className=" flex justify-between items-center font-semibold text-lg gap-4">
+              <li>
+                <NavLink href={'/'}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink href={'/about'}>About Us</NavLink>
+              </li>
+              <li>
+                <NavLink href={'/services'}>Services</NavLink>
+              </li>
+              <li>
+                <NavLink href={'/pricing'}>Pricing</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </>
   );
--- skat_babershop/components/ui/carousel.jsx
+++ skat_babershop/components/ui/carousel.jsx
@@ -1,40 +1,40 @@
-
 import React, { useState } from 'react';
-import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 const Carousel = (props) => {
-  const [current, setCurrent] = useState(0)
-  const { slides } = props
+  const [current, setCurrent] = useState(0);
+  const { slides } = props;
 
   const previousSlide = () => {
-    if ( current === 0) setCurrent(slides.length - 1);
-    else setCurrent(current - 1)
-  }
+    if (current === 0) setCurrent(slides.length - 1);
+    else setCurrent(current - 1);
+  };
 
   const nextSlide = () => {
-    if ( current === slides.length - 1) setCurrent(0);
-    else setCurrent(current + 1)
-  }
+    if (current === slides.length - 1) setCurrent(0);
+    else setCurrent(current + 1);
+  };
 
   return (
-    <div className=' overflow-hidden relative '>
-      <div className={" flex transition ease-out duration-40 "}
-      style={{
-        transform: `translateX(-${current * 100 }%)`,
-      }}
+    <div className=" overflow-hidden relative ">
+      <div
+        className={' flex transition ease-out duration-40 '}
+        style={{
+          transform: `translateX(-${current * 100}%)`,
+        }}
       >
-        {
-          slides.map((slide)=> {
-            return <img src={`images/slides/${slide}`} alt='slide' />
-          })
-        }
+        {slides.map((slide) => {
+          return (
+            <img src={`/images/slides/${slide}`} key={slide} alt="slide" />
+          );
+        })}
       </div>
-      <div className='flex justify-between absolute top-0 h-full w-full items-center'>
-        <button type='button' onClick={previousSlide}>
-          <FaArrowCircleLeft size={50}/>
+      <div className="flex justify-between absolute top-0 h-full w-full items-center">
+        <button type="button" onClick={previousSlide}>
+          <FaArrowCircleLeft size={50} />
         </button>
-        <button type='button' onClick={nextSlide}>
-          <FaArrowCircleRight size={50}/>
+        <button type="button" onClick={nextSlide}>
+          <FaArrowCircleRight size={50} />
         </button>
       </div>
     </div>
